6bb7c218521a6ddba469d4fb35d7e9446cc0241e
Yosmel López Pimentel <yosmellopez@gmail.com>
2020-02-24 16:05:23 -0500

    Actualizando algunas versiones de PHP

--- frontend/app/account/account.route.ts
+++ frontend/app/account/account.route.ts
@@ -1,16 +1,21 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
-import { activateRoute } from './activate/activate.route';
-import { passwordRoute } from './password/password.route';
-import { passwordResetFinishRoute } from './password-reset/finish/password-reset-finish.route';
-import { passwordResetInitRoute } from './password-reset/init/password-reset-init.route';
-import { settingsRoute } from './settings/settings.route';
+import {activateRoute} from './activate/activate.route';
+import {passwordRoute} from './password/password.route';
+import {passwordResetFinishRoute} from './password-reset/finish/password-reset-finish.route';
+import {passwordResetInitRoute} from './password-reset/init/password-reset-init.route';
+import {settingsRoute} from './settings/settings.route';
+import {AccountComponent} from "app/account/account.component";
+import {navbarRoute} from "app/layouts/navbar/navbar.route";
+import {errorRoute} from "app/layouts/error/error.route";
 
-const ACCOUNT_ROUTES = [activateRoute, passwordRoute, passwordResetFinishRoute, passwordResetInitRoute, settingsRoute];
+const LAYOUT_ROUTES = [navbarRoute, ...errorRoute];
+const ACCOUNT_ROUTES = [activateRoute, passwordRoute, passwordResetFinishRoute, passwordResetInitRoute, settingsRoute, ...LAYOUT_ROUTES];
 
 export const accountState: Routes = [
     {
         path: '',
+        component: AccountComponent,
         children: ACCOUNT_ROUTES
     }
 ];
--- public/app/global.bundle.js
+++ public/app/global.bundle.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5bcd29e56520f0fa4f19";
+/******/ 	var hotCurrentHash = "5e59baeeb696649e4c1d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
