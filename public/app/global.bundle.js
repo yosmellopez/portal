1d1967fe3a351e998831a0c00d7b157b95b70219
Yosmel López Pimentel <yosmellopez@gmail.com>
2020-02-25 17:38:31 -0500

    Terminando cambio de contrasena

--- public/app/global.bundle.js
+++ public/app/global.bundle.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "cb7d022ba65e66782bc0";
+/******/ 	var hotCurrentHash = "1abe916599ef1ee6edb5";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
