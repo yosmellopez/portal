620f019eb42fde28cc307ff9c686423c50d20f64
Yosmel Lopez Pimentel <yosmellopez@gmail.com>
2020-04-18 19:01:05 -0500

    Finalizando el portal.

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -69,20 +69,20 @@ class SearchController extends Controller
     public function listNumeroSerie(Request $request)
     {
         $tipoDocumento = $request->tipo;
-        $documentos = Documento::select("tipoDoc")->where('tipoDoc', $this->findTipoDoc($tipoDocumento))->get();
-        $collection = collect($documentos);
-        $seriesDocumentos = $collection->map(function ($item, $key) {
-            $numSerie = preg_split("/[-]/", $item->numSerie);
-            $pos = 0;
-            if (count($numSerie) > 2 && $numSerie[0] != "RC") {
-                $pos = 1;
-            }
-            return array("serie" => $numSerie[$pos]);
-        })->unique("serie")->toArray();
-        $series = array();
-        foreach ($seriesDocumentos as $serie) {
-            $series[] = $serie;
-        }
+        $series = Documento::select("serie")->where('tipoDoc', $this->findTipoDoc($tipoDocumento))->distinct('serie')->get();
+//        $collection = collect($documentos);
+//        $seriesDocumentos = $collection->map(function ($item, $key) {
+//            $numSerie = preg_split("/[-]/", $item->numSerie);
+//            $pos = 0;
+//            if (count($numSerie) > 2 && $numSerie[0] != "RC") {
+//                $pos = 1;
+//            }
+//            return array("serie" => $numSerie[$pos]);
+//        })->unique("serie")->toArray();
+//        $series = array();
+//        foreach ($seriesDocumentos as $serie) {
+//            $series[] = $serie;
+//        }
         return response()->json($series, 200);
     }
 
