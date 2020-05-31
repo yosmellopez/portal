6e64e0b2cdbbdc4d000854c9d72d9002ca291e4b
Yosmel Lopez Pimentel <yosmellopez@gmail.com>
2020-05-31 01:18:56 -0500

    Espero que funcione el calendar en espanol

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -15,7 +15,7 @@ const commonConfig = require('./webpack.common.js');
 const ENV = 'production';
 const sass = require('sass');
 
-module.exports = webpackMerge(commonConfig({ env: ENV }), {
+module.exports = webpackMerge(commonConfig({env: ENV}), {
     // Enable source maps. Please note that this will slow down the build.
     // You have to enable it in Terser config below and in tsconfig-aot.json as well
     // devtool: 'source-map',
@@ -34,49 +34,49 @@ module.exports = webpackMerge(commonConfig({ env: ENV }), {
             test: /(?:\.ngfactory\.js|\.ngstyle\.js|\.ts)$/,
             loader: '@ngtools/webpack'
         },
-        {
-            test: /\.scss$/,
-            use: ['to-string-loader', 'css-loader', {
-                loader: 'sass-loader',
-                options: { implementation: sass }
-            }],
-            exclude: /(vendor\.scss|global\.scss)/
-        },
-        {
-            test: /(vendor\.scss|global\.scss)/,
-            use: [
-                {
-                    loader: MiniCssExtractPlugin.loader,
-                    options: {
-                        publicPath: '../'
-                    }
-                },
-                'css-loader',
-                'postcss-loader',
-                {
+            {
+                test: /\.scss$/,
+                use: ['to-string-loader', 'css-loader', {
                     loader: 'sass-loader',
-                    options: { implementation: sass }
-                }
-            ]
-        },
-        {
-            test: /\.css$/,
-            use: ['to-string-loader', 'css-loader'],
-            exclude: /(vendor\.css|global\.css)/
-        },
-        {
-            test: /(vendor\.css|global\.css)/,
-            use: [
-                {
-                    loader: MiniCssExtractPlugin.loader,
-                    options: {
-                        publicPath: '../'
+                    options: {implementation: sass}
+                }],
+                exclude: /(vendor\.scss|global\.scss)/
+            },
+            {
+                test: /(vendor\.scss|global\.scss)/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: '../'
+                        }
+                    },
+                    'css-loader',
+                    'postcss-loader',
+                    {
+                        loader: 'sass-loader',
+                        options: {implementation: sass}
                     }
-                },
-                'css-loader',
-                'postcss-loader'
-            ]
-        }]
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: ['to-string-loader', 'css-loader'],
+                exclude: /(vendor\.css|global\.css)/
+            },
+            {
+                test: /(vendor\.css|global\.css)/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: '../'
+                        }
+                    },
+                    'css-loader',
+                    'postcss-loader'
+                ]
+            }]
     },
     optimization: {
         runtimeChunk: false,
@@ -131,7 +131,7 @@ module.exports = webpackMerge(commonConfig({ env: ENV }), {
         }),
         new MomentLocalesPlugin({
             localesToKeep: [
-                // jhipster-needle-i18n-language-moment-webpack - JHipster will add/remove languages in this array
+                'es', 'es-mx'
             ]
         }),
         new Visualizer({
@@ -148,8 +148,8 @@ module.exports = webpackMerge(commonConfig({ env: ENV }), {
             debug: false
         }),
         new WorkboxPlugin.GenerateSW({
-          clientsClaim: true,
-          skipWaiting: true,
+            clientsClaim: true,
+            skipWaiting: true,
         })
     ],
     mode: 'production'
